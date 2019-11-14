4b00b0d8fd6e3321f01a3c776879ca37ffcd7319
Soo Bin Hwang <chaotic521@hotmail.com>
2019-11-14 16:25:20 -0500

    Search and filter completed

--- soobin_code/static/js/app.js
+++ soobin_code/static/js/app.js
@@ -10,7 +10,12 @@ function createTable() {
         var row = tbody.append('tr');
         Object.entries(courtCase).forEach(([key, value]) => {
             var cell = row.append('td');
-            cell.text(value);
+            if (key != 'url') {
+                cell.text(value);
+            }
+            else {
+                cell.html('<a target="_blank" href=' + value + '>' + value + '</a>');
+            }            
         });
     });
 };
@@ -19,14 +24,37 @@ createTable();
 
 
 // Store search inputs.
-var searchTerm = d3.select('#searchTerm');
-console.log(searchTerm);
+var searchInput= d3.select('#searchTerm')
 
 // Listen events.
 var button = d3.select('#searchBtn');
 
 button.on('click', function() {
-    var searchValue = searchTerm.property('value').toLowerCase();
-    console.log(searchValue)
+    var searchTerm = searchInput.property('value').toLowerCase();
 
+    // Search the term within strings.
+    var filteredCase = courtData.filter((courtCase) => courtCase['top_tfidf'].indexOf(searchTerm) > -1);
+
+    tbody.html('');
+
+    filteredCase.forEach((courtCase) => {
+        var row = tbody.append('tr');
+        Object.entries(courtCase).forEach(([key, value]) => {
+            var cell = row.append('td');
+            if (key != 'url') {
+                cell.text(value);
+            }
+            else {
+                cell.html('<a target="_blank" href=' + value + '>' + value + '</a>');
+            }
+        })
+    });
+});
+
+// Reset the table.
+var buttonR = d3.select('#resetBtn');
+
+buttonR.on('click', function() {
+    tbody.html('');
+    createTable();
 });
